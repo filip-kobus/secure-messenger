9827d44f1e03a35c8f16641c08de30ea84ee7241
Filip <kobusfilip@outlook.com>
2025-10-31 20:13:17 +0100

    feat: auth endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,15 @@
 from fastapi import Depends, FastAPI
 
-from .dependencies import get_query_token, get_token_header
-from .internal import admin
-from .routers import items, users
+from app.dependencies import get_query_token, get_token_header
+from app.internal import admin
+from app.routers import items, users, auth
 
 app = FastAPI(dependencies=[Depends(get_query_token)])
 
 
 app.include_router(users.router)
 app.include_router(items.router)
+app.include_router(auth.router)
 app.include_router(
     admin.router,
     prefix="/admin",
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
+from app.schemas.auth import LoginRequest, RegisterRequest
 
 router = APIRouter()
 
-# @router.post("/login/", tags=["auth"])
-# async def login(login_data: dict):
+@router.post("/login/", tags=["auth"])
+async def login(login_data: LoginRequest):
+    return {"message": "User logged in successfully"}
+
+@router.post("/register/", tags=["auth"])
+async def register(register_data: RegisterRequest):
+    return {"message": "User registered successfully"}
